1894fde24b635503cbdc2e5d3df9b7ac91ba7022
srpjv <julien.vionb@gmail.com>
2021-07-06 02:16:24 +0200

    goupil

---
+++ Exo/import time.py
@@ -0,0 +1,12 @@
+﻿import datetime 
+
+dt = datetime.datetime.now()
+d0 = datetime.date(dt.year, dt.month, dt.day)
+
+print(d0)
+t=datetime.date.today()
+d1 = datetime.datetime(t.year, t.month, t.day)
+print(d1)
+
+print(datetime.datetime.min.time)
+05/05/28
