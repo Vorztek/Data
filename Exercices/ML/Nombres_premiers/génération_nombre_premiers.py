0ab135c02bba66cc3423734d96a7e9ddcff38649
srpjv <julien.vionb@gmail.com>
2021-07-29 03:41:45 +0200

    update

---
+++ Exercices/ML/Nombres_premiers/génération_nombre_premiers.py
@@ -0,0 +1,35 @@
+﻿import matplotlib.pyplot as plt
+
+meoisation = [0]
+liste1 = []
+
+def is_prime(p):
+    for j in range(2, p):
+        if p % j == 0:
+            return False
+    else:
+        meoisation.append(p)
+        return True, meoisation
+
+def number_of_prime(n):
+    compte = 0
+    for i in range(2, n):
+        if i not in meoisation:
+            if is_prime(i) is True:
+                compte += 1
+        else:
+            compte += 1
+    pourcent = ((compte / n) *100)
+    return pourcent
+
+def courbe():
+    for n in range(5, 500):
+        liste1.append(number_of_prime(n))
+    return liste1
+
+plt.plot(courbe())
+plt.show()
+
+
+
+
